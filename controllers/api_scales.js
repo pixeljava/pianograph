9c82152ee51d8fadee76cfe615ae66655ea43e1f
Jeremy Penning <4085868+pixeljava@users.noreply.github.com>
2020-11-17 21:18:26 -0800

    Updated look and logic of modal windows. Refactored fonts folder to include FontAwesome. Worked on update root note form logic.

--- controllers/api_scales.js
+++ controllers/api_scales.js
@@ -1,71 +1,153 @@
 const express = require('express');
-const db = require('../db/server');
-const RootNote = require('../db/models/Rootnote');
+const Scales = require('../db/models/Scales');
 
 const router = express.Router();
 
-// GET /api/rootnotes/
-// GET a list of all root notes when accessing <host>/api/rootnotes/
+router.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
+// GET /api/scales/
+// GET a list of all root notes when accessing <host>/api/scales/
 router.get('/', (req, res) => {
-  RootNote.findAll()
-    .then(rootnotes => {
-      res.status(201).send(rootnotes);
+  Scales.findAll()
+    .then(scales => {
+      res.status(200).json(scales);
     })
     .catch(e => {
       console.log(e);
-      res.status(500).send(e);
+      res.status(500).json(e);
     });
 });
 
-// Add a root note to the root_note table
-router.get('/add', (req, res) => {
-  let { title, wikiurl, wikipageid, binposition, numposition } = req.body;
+// POST /api/scales/add
+// POST a new root note with: title, wikiurl, binposition, numposition, rootnoteId
+router.post('/add', (req, res) => {
   let errors = {};
+  let request = req.body;
 
-  if(!title) {
-    errors.title = { text: 'Please add a title' };
+  if(!request.title) {
+    errors.fields.title = 'Please add a title';
   }
-  if(!wikiurl) {
-    errors.wikiurl = { text: 'Please add a Wikipedia URL for this note' };
+  if(!request.wikiurl) {
+    errors.fields.wikiurl = 'Please add a Wikipedia URL for this note';
   }
-  if(!binposition) {
-    errors.binposition = { text: 'Please add PianoGraph binary position' };
+  if(!request.binposition) {
+    errors.fields.binposition = 'Please add PianoGraph binary position';
   }
-  if(!numposition) {
-    errors.numposition = { text: 'Please add PianoGraph numeric position' };
+  if(!request.numposition) {
+    errors.fields.numposition = 'Please add PianoGraph numeric position';
+  }
+  if(!request.rootnoteId) {
+    errors.fields.rootnoteId = 'Please add the id of the root note that this scale refers to';
   }
 
   if(Object.keys(errors).length > 0) {
     res.status(500).send({
       errors,
-      title, 
-      wikiurl, 
-      wikipageid, 
-      binposition, 
-      numposition
+      title: request.title,
+      wikiurl: request.wikiurl,
+      wikipageid: request.wikipageid,
+      binposition: request.binposition,
+      numposition: request.numposition,
+      rootnoteId: request.rootnoteId
     });
   } else {
-  // Insert all fields into the rootnotes table
-  RootNote.create({
-    title,
-    wikiurl,
-    wikipageid,
-    binposition,
-    numposition
+    Scales.create({
+      title: request.title,
+      wikiurl: request.wikiurl,
+      wikipageid: request.wikipageid,
+      binposition: request.binposition,
+      numposition: request.numposition,
+      rootnoteId: request.rootnoteId
+    })
+    .then(returnModel => {
+      res.status(201).send(returnModel);
+    })
+    .catch(e => {
+      res.status(500).send(e);
+    });
+  }
+});
+
+// PUT /api/scales/add
+// PUT an updated scale {id} with: title, wikiurl, binposition, numposition, rootnoteId
+router.put('/update/:scaleId', (req, res) => {
+  let errors = {};
+  let request = req.body;
+  console.log(req.body);
+
+  if(!request.title) {
+    errors.fields.title = 'Please add a title';
+  }
+  if(!request.wikiurl) {
+    errors.fields.wikiurl = 'Please add a Wikipedia URL for this note';
+  }
+  if(!request.binposition) {
+    errors.fields.binposition = 'Please add PianoGraph binary position';
+  }
+  if(!request.numposition) {
+    errors.fields.numposition = 'Please add PianoGraph numeric position';
+  }
+  if(!request.rootnoteId) {
+    errors.fields.rootnoteId = 'Please add the id of the root note that this scale refers to';
+  }
+
+  if(Object.keys(errors).length > 0) {
+    res.status(500).send({
+      errors,
+      title: request.title,
+      wikiurl: request.wikiurl,
+      wikipageid: request.wikipageid,
+      binposition: request.binposition,
+      numposition: request.numposition,
+      rootnoteId: request.rootnoteId
+    });
+  } else {
+    Scales.update({
+      title: request.title,
+      wikiurl: request.wikiurl,
+      wikipageid: request.wikipageid,
+      binposition: request.binposition,
+      numposition: request.numposition
+    }, {
+      where: {id: req.params.scaleId},
+      returning: true
+    })
+    .then(function ([ rowsUpdated, [returnUpdateModel] ]) {
+        res.status(202).send(returnUpdateModel);
+      })
+    .catch(e => {
+      res.status(500).send(e);
+    });
+  }
+});
+
+// DELETE /api/scales/remove/{scaleId}
+// DELETE a scale {id}
+router.delete('/remove/:scaleId', (req, res) => {
+  let errors = {};
+  let request = req.body;
+
+  Scales.destroy({
+    where: {id: req.params.scaleId}
+  })
+  .then((num) => {
+    if(num === 1) {
+      console.log('num',num);
+      res.send({
+        message: "Scale was deleted successfully!"
+      });
+    } else {
+      res.send({
+        message: "Scale was not deleted. Try refreshing the page and trying again."
+      });
+    }
   })
   .catch(e => {
-    console.log(e);
-    res.status(500).send(
-      e,
-      errors,
-      title, 
-      wikiurl, 
-      wikipageid, 
-      binposition, 
-      numposition
-    );
+    console.log('e',e);
+    res.status(500).send(e);
   });
-  }
 
 });
 
--- public/js/rootnote.js
+++ public/js/rootnote.js
@@ -4,17 +4,20 @@ $( document ).ready(function() {
   const xhrUpdateNote = '/api/rootnotes/update'; // PUT note {id}
   const xhrDeleteNote = '/api/rootnotes/remove'; // DELETE note {id}
 
-  const updateNote = (id) => {
-    console.log(`Not quite updating id [${id}] yet...`);
-  };
-  const deleteNote = (id) => {
-    console.log(`Not quite deleting id [${id}] yet...`);
-  };
+  function objectifyForm(formArray) {
+    // Fix for POST/PUT
+    var returnArray = {};
+    for (var i = 0; i < formArray.length; i++){
+        returnArray[formArray[i]['name']] = formArray[i]['value'];
+    }
+    return returnArray;
+  }
   
   // GET all root notes and then update with view.
   const doGetNotes = () => {
+    // Clear the content box first
     $('section.boxContent').empty();
-
+    // Then fill it with the results of the AJAX call.
     $.ajax({
       type: 'GET',
       url: xhrGetNotes,
@@ -23,41 +26,178 @@ $( document ).ready(function() {
       success: function(result){
         $.each(result, function(i, data) {
           var rootNote = $(`
-            <div class="noteHolder">
-              <p>Note: ${this.title}</p>
-              <p>Wikipedia URL: <a href="${this.wikiurl}">${this.wikiurl}</a></p>
-              <p>Wikipedia PageId: ${this.wikipageid}</p>
-              <p>PianoGraph: ${this.binposition} (${this.numposition})</p>
-              <button id="update-${this.id}" data-object-id="${this.id}" title="Update Note">Update</button>
-              <button id="delete-${this.id}" data-object-id="${this.id}" title="Delete Note">Delete</button>
-            </div>`);
-          $(document).off().on('click', `#update-${this.id}`, function(e) {
-            e.preventDefault();
-            updateNote(data.id);
-          });
-          $(document).off().on('click', `#delete-${this.id}`, function(e) {
-            e.preventDefault();
-            doDeleteNote(e, data.id);
-          });
+            <div class="noteHolder"
+                 data-note-id="${this.id}" data-note-title="${this.title}"
+                 data-note-wikipageid="${this.wikipageid}" data-note-binposition="${this.binposition}">
+              <h1 class="noteTitle">Note: ${this.title}</h1>
+              <div class="noteInfo">
+                <p><strong>Wikipedia URL: </strong><a target="_blank" href="${this.wikiurl}">${this.wikiurl}</a></p>
+                <p><strong>Wikipedia PageId: </strong>${this.wikipageid}</p>
+                <p><strong>PianoGraph: </strong>${this.binposition} (${this.numposition})</p>
+              </div>
+              <div class="updateForm">
+                <div class="errorMessages">
+                  <p>This is an error message!</p>
+                  <p>This is an error message!</p>
+                  <p>This is an error message!</p>
+                  <p>This is an error message!</p>
+                </div>
+                <div class="formHolder">
+                  <form>
+                    <div class="formInput">
+                      <label for="title">Note Name:</label>
+                      <input type="text" id="title-${this.id}" placeholder="Enter note name..."
+                             name="title" value="${this.title}">
+                    </div>
+                    <br />
+                    <div class="formInput">
+                      <label for="binposition">Binary Representation:</label>
+                      <input type="text" id="binposition-${this.id}" placeholder="Enter binary..."
+                             name="binposition" value="${this.binposition}">
+                    </div>
+                    <br />
+                    <div class="formInput">
+                      <label for="numposition">Numerical Representation:</label>
+                      <input type="text" id="numposition-${this.id}" placeholder="Enter number..."
+                             name="numposition" value="${this.numposition}">
+                    </div>
+                    <br />
+                    <div class="formInput">
+                      <label for="wikiurl">Wikipedia Link:</label>
+                      <input type="text" id="wikiurl-${this.id}" placeholder="Enter URL..."
+                             name="wikiurl" value="${this.wikiurl}">
+                    </div>
+                    <br />
+                    <div class="formInput">
+                      <label for="wikipageid">Wikipedia Page Id:</label>
+                      <input type="text" id="wikipageid-${this.id}" placeholder="Enter id..."
+                             name="wikipageid" value="${this.wikipageid}">
+                    </div>
+                    <br />
+                    <button class="saveButton" id="saveNote-${this.id}" 
+                            type="button"><i class="far fa-save"></i> Save</button>
+                    <button class="cancelButton" id="cancelUpdate-${this.id}"
+                            type="button"><i class="fas fa-times-circle"></i> Cancel</button>
+                  </form>
+                </div>
+              </div>
+              <div class="bottomButtons">
+                <button class="viewButton" id="viewNote-${this.id}" title="View Note">
+                  <i class="fas fa-music"></i> View
+                </button>
+                <button class="scalesButton" id="viewScales-${this.id}" title="View Scales">
+                  <i class="fas fa-list-ul"></i> Scales
+                </button>
+                <div class="right">
+                  <button class="updateButton" id="update-${this.id}" title="Update Note">
+                    <i class="fas fa-pencil-alt"></i> Update
+                  </button>
+                  <button class="deleteButton" id="delete-${this.id}" title="Delete Note">
+                    <i class="fas fa-trash-alt"></i> Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          `);
+
           $(rootNote).appendTo('section.boxContent');
-        });        
+          // Event handlers for the updateForm 
+          $(`button#saveNote-${this.id}:button`).off().on('click', function (e) {
+            let note = $(this).closest('div.noteHolder');
+            let noteId = $(note).data('noteId');
+            let updateForm = $(note).find('div.updateForm form');
+            let data = $(updateForm).serializeArray();
+            data = objectifyForm(data);
+
+            doSaveNote(noteId, data);
+          });
+          $(`button#cancelUpdate-${this.id}:button`).off().on('click', function (e) {
+            let note = $(this).closest('div.noteHolder');
+            let updateForm = $(note).find('div.updateForm');
+            let bottomButtons = $(note).find('div.bottomButtons');
+            bottomButtons.toggle();
+            updateForm.slideToggle(250);
+          });
+
+          $(`button#viewNote-${this.id}:button`).off().on('click', function (e) {
+            //doViewNote(data.id);
+          });
+          $(`button#viewScales-${this.id}:button`).off().on('click', function (e) {
+            //doViewScales(data.id);
+          });
+          $(`button#update-${this.id}:button`).off().on('click', function (e) {
+            let note = $(this).closest('div.noteHolder');
+            let updateForm = $(note).find('div.updateForm');
+            let bottomButtons = $(note).find('div.bottomButtons');
+            bottomButtons.toggle();
+            updateForm.slideToggle(250);
+          });
+          $(`button#delete-${this.id}:button`).off().on('click', function (e) {
+            let note = $(this).closest('div.noteHolder');
+            let noteTitle = $(note).data('noteTitle');
+            let noteId = $(note).data('noteId');
+            modal.open({content: $(`
+              <h1 id="modalHeader">Confirm Delete</h1>
+              <p id="modalText">Are you sure you want to delete root note: <strong>${noteTitle}</strong>?</p>
+              <button id="modalConfirm" class="modalButton red" title="Delete ${noteTitle}">
+                <i class="fas fa-trash-alt"></i> Delete
+              </button>
+              <button id="modalCancel" class="modalButton grey right" title="Cancel">
+                <i class="fas fa-times-circle"></i> Cancel
+              </button>
+              `), width: 'max(80vw, 300px)'});
+            // Set event listeners to the buttons
+            $(`button#modalConfirm:button`).off().on('click', function (e) {
+              doDeleteNote(noteId);
+            });
+            $(`button#modalCancel:button`).off().on('click', function (e) {
+              modal.close();
+            });
+            // Move this behind a modal confirmation.
+          });
+
+        }); // End each()
       }
     });
   }; 
   doGetNotes(); // Just run this automatically once the document is ready...
 
-  // DELETE root notes {id} and then update the view.
-  const doDeleteNote = (e, id) => {
-    console.log('e:', e);
+
+
+  // DELETE deletes root notes {id} and then update the view.
+  const doDeleteNote = (noteId) => {
+    console.log(`id: ${noteId}`);
     $.ajax({
       type: 'DELETE',
-      url: `${xhrDeleteNote}/${id}`,
+      url: `${xhrDeleteNote}/${noteId}`,
       contentType: 'application/json',
       dataType: 'json',
+      success: function(result){
+        modal.close();
+        doGetNotes();
+      }
+    });
+  };
+
+  // PUT updates root notes {id} and then update the view.
+  const doSaveNote = (noteId, updateData) => {
+    console.log(`noteId: ${noteId}`);
+    console.log('updateData: ', updateData);
+    $.ajax({
+      type: 'PUT',
+      url: `${xhrUpdateNote}/${noteId}`,
+      contentType: 'application/x-www-form-urlencoded',
+      data: updateData,
       success: function(result){
         doGetNotes();
       }
     });
+  };
+  const doViewNote = (e, id) => {
+    console.log('e:', e);
+  };
+  const doViewScales = (e, id) => {
+    console.log('e:', e);
   };
 
 });
